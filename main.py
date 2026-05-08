fcdf5496fb7d64aa1e4f86836032770f36398ea2
Noah Paden <noahtpc@gmail.com>
2026-05-08 01:44:34 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import smtplib
+
+sender_email = "you@gmail.com"
+receiver_emails = ["recipient1@gmail.com","recipient2@gmail.com","recipient3@gmail.com"]
+
+message = f"Subject: insert_subject\n\ninsert_email_body"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, "your_account_id")
+
+for j in range(len(receiver_emails)):
+        server.sendmail(sender_email, receiver_emails[j], message)
